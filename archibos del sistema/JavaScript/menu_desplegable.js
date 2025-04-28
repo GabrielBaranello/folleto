2d152d0f08a5aa70d62e6ae1dc6eb28f7def6ba2
Gabi <Gabrielbaranello3d@gmail.com>
2025-04-28 19:58:15 -0300

    Delete archibos del sistema directory

--- archibos del sistema/JavaScript/menu_desplegable.js
+++
@@ -1,8 +0,0 @@
-const dropdown = document.getElementById('dropdown');
-const image = document.getElementById('image');
-
-dropdown.addEventListener('change', (event) => {
-    // Cambiar la imagen según la selección
-    const selectedValue = event.target.value;
-    image.src = selectedValue; // Cambia la ruta de la imagen
-});
