f1322dcd03dff08cee088a567a3d7bfc7cf0a0d4
Gabi <gabrielbaranello3d@gmail.com>
2025-05-01 18:53:21 -0300

    Adaptando todo para funcionar mejor

--- archibos del sistema/JavaScript/precios.js
+++ archibos del sistema/JavaScript/precios.js
@@ -1,32 +1,28 @@
 // Definir un objeto con valores asociados a cada archivo HTML
 const valoresPorArchivo = {
     "auriculares.html": "$50",
-    "mem ram 8 gb.html": "$100",
+    "cables.html": "$50",
+    "fuente.html": "$100",
+    "mem_ram_8_gb.html": "$100",
+    "mem_ram_16_gb.html": "$150",
     "mem ram 16 gb.html": "$150",
-    "auriculares.html": "$50",
-    "mem ram 8 gb.html": "$100",
-    "mem ram 16 gb.html": "$150",
-    "auriculares.html": "$50",
-    "mem ram 8 gb.html": "$100",
-    "mem ram 16 gb.html": "$150",
-    "auriculares.html": "$50",
-    "mem ram 8 gb.html": "$100",
-    "mem ram 16 gb.html": "$150",
-    "auriculares.html": "$50",
-    "mem ram 8 gb.html": "$100",
-    "mem ram 16 gb.html": "$150",
-    "auriculares.html": "$50",
-    "auriculares.html": "$50",
-    "mem_ram_8gb.html": "$100",
-    "mem_ram_16gb.html": "$150"
+    "mem_rom.html": "$100",
+    "monitor.html": "$50",
+    "mouse_corriente.html": "$150",
+    "mouse_gamer_rgb.html": "$50",
+    "parlantes.html": "$100",
+    "placa_madre.html": "$150",
+    "prosesador.html": "$50",
+    "taclas.html": "$100",
+    "tira_led_rgb.html": "$150",
+    "ventilador_rgb": "$50",
+    "ventilador": "$50"
     // Agrega más archivos y valores según sea necesario
 };
 
-// Obtener el nombre del archivo actual
 let nombreArchivo = window.location.pathname.split("/").pop();
-console.log("Archivo cargado:", nombreArchivo); // Depuración
+console.log("Archivo cargado:", nombreArchivo);
 
-// Buscar el elemento h2 por su id y asignar el valor correspondiente
 let titulo = document.getElementById("titulo");
 if (titulo && valoresPorArchivo[nombreArchivo]) {
     titulo.textContent = valoresPorArchivo[nombreArchivo];
